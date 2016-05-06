5fdd1ca623a9a90cfe7a44d8629cb266bb75dadb
ErickxDD <erickrachell@gmail.com>
2016-05-06 09:45:52 -0600

    arrayPersonas=dbpersonas

--- Examen 1/src/Modelo/ArrayPersonas.java
+++ Examen 1/src/Modelo/ArrayPersonas.java
@@ -4,12 +4,54 @@
  * and open the template in the editor.
  */
 package Modelo;
-
-/**
+import java.util.ArrayList;
+/*
  *
  * @author Erick
  */
 public class ArrayPersonas {
     
+    private ArrayList<Persona> arregloPersona;
+    private String matrizDatos[][];
+
+    public ArrayPersonas() {
+
+        this.arregloPersona = new ArrayList<Persona>();
+    }
+
+    public void setArregloPersona(ArrayList<Persona> arregloPersona) {
+        this.arregloPersona = arregloPersona;
+    }
+
+    public String agregarEstudiante(Persona persona) throws PersonaException {
+
+        if (verificarPersona(persona.getIdentificacion())) 
+            throw new PersonaException(
+                    "Ya existe una persona con ese numero de cedula ",
+                    false);
+        
+        arregloPersona.add(persona);
+        return "Se agrego con exito";
+    }
+
+    public boolean verificarPersona(String identificacion) {
+
+        boolean dev = false;
+        if (arregloPersona.size() != 0) {
+
+            for (int i = 0; i < arregloPersona.size(); i++) {
+                if (arregloPersona.get(i).getIdentificacion().equalsIgnoreCase(identificacion)) {
+
+                    dev = true;
+
+                    break;
+
+                }
+            }
+
+        }
+        return dev;
+    }
+
     
 }
---
+++ Examen 1/src/Modelo/PersonaException.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author Erick
+ */
+public class PersonaException extends Exception {
+    
+    private boolean limpiarInterfaz;
+
+    public PersonaException(String message, boolean limpiarInterfaz) {
+        super(message);
+        this.limpiarInterfaz = limpiarInterfaz;
+    }
+
+    public boolean isLimpiarInterfaz() {
+        return limpiarInterfaz;
+    }
+    
+}
+    
+}
